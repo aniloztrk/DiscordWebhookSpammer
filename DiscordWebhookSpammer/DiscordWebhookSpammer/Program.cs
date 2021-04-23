99aeed78781f500cb2bf1527a37476bf1e46234b
Fahrettin Enes <76036578+fahrettinenes@users.noreply.github.com>
2021-04-23 14:46:02 +0300

    General Refactoring

--- DiscordWebhookSpammer/DiscordWebhookSpammer/Program.cs
+++ DiscordWebhookSpammer/DiscordWebhookSpammer/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Net;
-using System.Text;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Net;
 
 namespace DiscordWebhookSpammer
 {
@@ -13,55 +9,37 @@ namespace DiscordWebhookSpammer
     {
         static void Main()
         {
-            string webhook, webhookname, message;
-            int spamcount;
             Console.WriteLine("Welcome Mixy Discord Webhook Spammer.", Console.ForegroundColor = ConsoleColor.Red);
             Console.WriteLine("Enter your webhook.", Console.ForegroundColor = ConsoleColor.Yellow);
-            webhook = Console.ReadLine();
+            var webhook = Console.ReadLine();
             Console.WriteLine("Enter your webhook name.");
-            webhookname = Console.ReadLine();
+            var webhookname = Console.ReadLine();
             Console.WriteLine("Enter message.");
-            message = Console.ReadLine();
+            var message = Console.ReadLine();
             Console.WriteLine("Enter how many times do you send a message.");
-            spamcount = Convert.ToInt32(Console.ReadLine());
+            var spamcount = Convert.ToInt32(Console.ReadLine());
             Console.ResetColor();
             for (int i = 0; i < spamcount; i++)
             {
-                Thread.Sleep(2000);
-                Discord.Send(message, webhook, webhookname, "");
+                Send(message, webhook, webhookname);
                 Console.WriteLine(i.ToString(), Console.ForegroundColor = ConsoleColor.Cyan);
+                Thread.Sleep(2000);
             }
+            Console.WriteLine("Finish!")
             Console.ReadKey();
         }
-    }
-
-    public class Discord
-    {
-        public class Http
+        
+        public static void Send(string content, string webhookUrl, string username)
         {
-            public static byte[] Post(string url, NameValueCollection pairs)
-            {
-                using (WebClient webClient = new WebClient())
-                    return webClient.UploadValues(url, pairs);
-            }
-        }
-        public static void Send(string content, string webHookUrl, string username, string avatarUrl)
-        {
-            Http.Post(webHookUrl, new NameValueCollection()
+            wc.UploadValues(webhookUrl, new NameValueCollection()
             {
                 {
                     "content", content
                 },
-
                 {
                     "username", username
                 },
-
-                {
-                    "avatar_url", avatarUrl
-                }
-
-            });
+            }
         }
     }
 }
